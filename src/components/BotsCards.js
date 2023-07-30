ba106da1c660087216bd6426b46c8a9dc02916ae
peterkomo <mwangipeterkomo@gmail.com>
2023-07-31 00:11:50 +0300

    handle clicking on the is added

--- src/components/BotsCards.js
+++ src/components/BotsCards.js
@@ -1,3 +1,22 @@
 import React from "react";
 // botsCard componenet reseaving many props
-const BotsCards = ({ bot, enlistBot, releaseBot, dischargeBot, isEnlisted }) => {}
+const BotsCards = ({ bot, enlistBot, releaseBot, dischargeBot, isEnlisted }) => {
+    // destructuring the properties of the bot passed  as a prop
+    const {
+        id,
+        name,
+        health,
+        damage,
+        armor,
+        bot_class,
+        catchphrase,
+        avatar_url,
+      } = bot;
+    // function to handle click on the bot card
+      const handleClick = () => {
+        if (isEnlisted) {
+          releaseBot(id); // If already enlisted, release the bot
+        } else {
+          enlistBot(id); // If not enlisted, enlist the bot
+        }
+      };
