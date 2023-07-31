a5a29fe921cb4986f1e4622252fb755ad56caa7c
peterkomo <mwangipeterkomo@gmail.com>
2023-07-31 09:12:03 +0300

    added the return value of the jsx,

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React,{useState,useEffect} from "react";
 import BotsCards from "./components/BotsCards";
 import BotArmy from "./components/BotArmy";
+import CollectionOfBots from "./components/CollectonOfBots";
 
 
 const App = () => {
@@ -20,3 +21,59 @@ const [yourBotArmy, setYourBotArmy] = useState([]);
 }, []);
 //The component uses the useState hook to manage two pieces of state: bots (containing all bots) and yourBotArmy (containing enlisted bots).
 // The useEffect hook is used to fetch the list of bots from the backend when the component mounts for the first time. It performs an HTTP GET request to the provided API endpoint using fetch.
+
+//enlisting realising,and discharging chat bots
+const enlistBot = (botId) => {
+  const botToEnlist = bots.find((bot) => bot.id === botId);
+  if (botToEnlist && !yourBotArmy.some((bot) => bot.id === botId)) {
+    setYourBotArmy([...yourBotArmy, botToEnlist]);
+  }
+};
+//find is for geting a specific charachter
+//some is to give a specific charachter an action
+// Function to release a bot from yourBotArmy
+const releaseBot = (botId) => {
+  const updatedBotArmy = yourBotArmy.filter((bot) => bot.id !== botId);
+  setYourBotArmy(updatedBotArmy);
+};
+// Function to discharge a bot (remove from yourBotArmy and backend)
+const dischargeBot = (botId) => {
+  // Delete the bot from the backend
+  fetch(`https://api.npoint.io/725e93b04b51723612b0/bots/${botId}`, {
+    method: "DELETE",
+  })
+    .then((response) => {
+      if (response.ok) {
+        // If successfully deleted from the backend, remove the bot from yourBotArmy state
+        const updatedBotArmy = yourBotArmy.filter((bot) => bot.id !== botId);
+        setYourBotArmy(updatedBotArmy);
+      } else {
+        console.error("Error discharging bot:", response);
+      }
+    })
+    .catch((error) => {
+      console.error("Error discharging bot:", error);
+    });
+};
+//enlistBot function is used to add a bot to yourBotArmy. It first checks if the bot with the specified ID exists in the list of all bots (bots) and whether it is not 
+// rendering components with the correct props 
+return (
+  <div>
+    <h1 className="mt-3">Welcome to Flatiron Bots</h1>
+    <h2>My Hack Bot</h2>
+    <BotArmy
+      yourBotArmy={yourBotArmy}
+      releaseBot={releaseBot}
+      dischargeBot={dischargeBot}
+    />
+    <h2>Crew Bots</h2>
+    <CollectionOfBots
+      bots={bots}
+      enlistBot={enlistBot}
+      yourBotArmy={yourBotArmy}
+      releaseBot={releaseBot} // Pass the releaseBot function to BotCollection
+      dischargeBot={dischargeBot} // Pass the dischargeBot function to BotCollection
+    />
+  </div>
+);
+};
